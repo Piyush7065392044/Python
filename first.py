c33a056392b4d2e682b7f62145b9316eea2f3752
Piyush7065392044 <129537005+Piyush7065392044@users.noreply.github.com>
2025-03-18 02:02:33 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,90 @@
+# for i in range (1, 11):
+#     # print("piyush"+ i )
+#     print(str(i)+""+"  piyush")
+
+# x = 0 
+# while x<=10:
+#     print(x)
+#     x= x+1
+
+# x =int  (input("enter the value of x "))
+# if x >= 10:
+#     print("able for vote")
+# else: print("not able ")
+  
+# x, y = map(int, input("Enter the value of x and y: ").split())  
+# print(x + y)
+
+
+# x = int(input("enter the value of x "))
+# if x <= 10 :
+#     print("number 10 ")
+# elif x >= 100:
+#     print("number 100")
+# elif x > 1000:
+#     print("number 1000")
+# else: 
+#     print("not avalible ")
+
+
+# function
+# def add_number(a,b):
+#     a = int(input("enter the value of a"))
+#     b = int(input("enter the value of b"))
+   
+#     return a * b
+
+# result = add_number(10,10 )
+# print(result )
+# def greet ():
+#    name = input("enter your  name ")
+#    print("hello"+name)
+
+
+
+
+# greet()
+# arr_list =[1,2,3,4,5]
+# print(arr_list)
+
+# forloop_list= [i for i in range(1,11)]
+# even_number = [i for i in forloop_list if i %2 == 0]
+
+# print(forloop_list)
+
+# explain 
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     return n * factorial(n-1)
+
+# print(factorial(5))  # Output: 120
+
+
+
+# explain 
+# Normal function
+# def square(x):
+#     return x*x
+
+# # Lambda function
+# square_lambda = lambda x: x*x
+
+# print(square(5))         # Output: 25
+# print(square_lambda(5))  # Output: 25
+# name = ' jhon'
+# nameone= "chena"
+# print(name+nameone)
+
+# age = 16 
+# print (age)
+# superheloname =input("enter your name ")
+# print("piysuh" + ""+   superheloname)
+# type converion
+# old_age = input("enter age ")
+# new_age = int(old_age) +2
+# print(new_age)
+frist = input("1 ")
+second = input("2")
+sum = frist + second
+print('the sum is '+sum)
